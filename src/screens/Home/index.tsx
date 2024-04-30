c1b54c5767b53eea0edb6b8a8feefe7752feb645
alexpacheli <alex@pacheli.com.br>
2024-04-29 23:18:40 -0300

    add `TextInput` for event name

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,4 +1,4 @@
-import { View, Text } from "react-native";
+import { View, Text, TextInput } from "react-native";
 
 import { styles } from "./styles";
 
@@ -7,6 +7,12 @@ export function Home() {
     <View style={styles.container}>
       <Text style={styles.eventName}>Nome do evento</Text>
       <Text style={styles.eventDate}>Sexta, 4 de novembro de 2022</Text>
+      <TextInput
+        style={styles.input}
+        placeholder="Nome do participante"
+        placeholderTextColor="#6B6B6B"
+        keyboardType="default"
+      />
     </View>
   );
 }
